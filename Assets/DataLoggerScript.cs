18074fa4d6613023df6d5b75cf54e5eb982c23c2
Minna Yu <minyu@chapman.edu>
2021-02-11 23:54:47 -0800

    Initial commit

---
+++ Assets/DataLoggerScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class DataLoggerScript : MonoBehaviour
+{
+    public string filename = "log.txt";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateFile();
+        OnApplicationStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        using (StreamWriter writer = File.AppendText(filename))
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                DateTime now = DateTime.Now;
+                writer.WriteLine(now.ToString() + "     Spacebar pressed");
+            }
+        }
+ 
+    }
+
+    public void OnClick()
+    {
+        using (StreamWriter writer = File.AppendText(filename))
+        {
+            DateTime now = DateTime.Now;
+            writer.WriteLine(now.ToString() + "     Button clicked");
+        }
+    }
+    private void OnApplicationStart()
+    {
+        //writes current time to file
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            DateTime now = DateTime.Now;
+            writer.WriteLine(now.ToString() + "     Application opened");
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        using (StreamWriter writer = File.AppendText(filename))
+        {
+            DateTime now = DateTime.Now;
+            writer.WriteLine(now.ToString() + "     Application closed");
+        }
+    }
+
+    private void CreateFile()
+    {
+        if(!File.Exists(filename))
+        {
+            File.Create(filename).Close();
+        }
+    }
+
+
+    
+}
